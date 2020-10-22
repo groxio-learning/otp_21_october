f8b6b05dcb49533d4c34c155cf5f9390cd026a57
sigu <sigu@podiihq.com>
2020-10-22 17:38:56 +0300

    enable move

--- mindex/lib/mindex/core/board.ex
+++ mindex/lib/mindex/core/board.ex
@@ -1,11 +1,9 @@
 defmodule Mindex.Core.Board do
-
   defstruct answer: [], guesses: []
 
-  def new(answer \\[1,2,3,4]), do: %__MODULE__{answer: answer}
+  def new(answer \\ [1, 2, 3, 4]), do: %__MODULE__{answer: answer}
 
   def move(board, guess) do
-
-    board
+    %{board | guesses: [guess | board.guesses]}
   end
 end
