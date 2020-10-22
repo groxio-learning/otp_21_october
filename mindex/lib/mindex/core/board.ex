9e38a6adba5f35230c095b081cbf7d93dee0a91a
Danilo <danilo.lima@sumup.com>
2020-10-22 12:05:51 -0300

    Fixed random answer generation

--- mindex/lib/mindex/core/board.ex
+++ mindex/lib/mindex/core/board.ex
@@ -1,7 +1,10 @@
 defmodule Mindex.Core.Board do
   defstruct answer: [], guesses: []
 
-  def new, do: %__MODULE__{answer: Enum.shuffle(1..4)}
+  def new do
+    random_answer = 1..8 |> Enum.shuffle() |> Enum.take(4)
+    %__MODULE__{answer: random_answer}
+  end
 
   def new([_a, _b, _c, _d] = answer), do: %__MODULE__{answer: answer}
 
